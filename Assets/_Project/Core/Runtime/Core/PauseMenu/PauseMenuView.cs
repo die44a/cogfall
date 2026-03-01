440cd45f2617b889d7c42578878eaba75d39306a
dima borisov <dim4ig2007@gmail.com>
2026-03-01 18:24:22 +0500

    add session MVVM and coordiantor

---
+++ Assets/_Project/Core/Runtime/Core/PauseMenu/PauseMenuView.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Project.Core.Runtime.Core.PauseMenu
+{
+    public class PauseMenuView : MonoBehaviour
+    {
+        [SerializeField] private Button _resumeButton;
+        [SerializeField] private Button _exitToMainMenuButton;
+
+        
+    }
+}
